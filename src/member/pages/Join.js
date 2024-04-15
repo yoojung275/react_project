cdfc5b4e669a834b83904d2be3b08d10f6cd2513
yoojung275 <yoojung275@gmail.com>
2024-04-15 17:50:47 +0900

    회원 가입 페이지 구성 중

--- src/member/pages/Join.js
+++ src/member/pages/Join.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import JoinForm from '../components/JoinForm';
+import { OuterBox } from '../../commons/components/LayoutBox';
 
 const Join = () => {
-  return <h1>회원 가입페이지</h1>;
+  const { t } = useTranslation();
+  return (
+    <>
+      <Helmet>
+        <title>{t('회원가입')}</title>
+      </Helmet>
+      <OuterBox>
+        <h1>{t('회원가입')}</h1>
+        <JoinForm />
+      </OuterBox>
+    </>
+  );
 };
 
 export default React.memo(Join);
